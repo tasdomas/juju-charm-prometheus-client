c3845c3fd28677c3c492211b12578388998ce1ab
Domas Monkus <domas@monkus.lt>
2016-05-06 15:29:57 +0300

    Init.

---
+++ provides.py
@@ -0,0 +1,25 @@
+from charms.reactive import RelationBase, scopes, hook
+
+class PrometheusProvides(RelationBase):
+    scope = scopes.UNIT
+
+    @hook('{provides:prometheus}-relation-{joined,changed}')
+    def changed(self):
+        self.set_state('{relation_name}.available')
+        
+    @hook('{provides:prometheus}-relation-{broken,departed}')
+    def broken(self):
+        self.remove_state('{relation_name}.available')
+        
+    def configure(self, port,path='/metrics', scrape_interval=None, scrape_timeout=None):
+        relation_info = {
+            'hostname': hookenv.unit_get('private-address'),
+            'port': port,
+            'metrics_path': path,
+        }
+        if scrape_interval is not None:
+            relation_info['scrape_interval'] = scrape_interval
+        if scrape_timeout is not None:
+            relation_info['scrape_timeout'] = scrape_timeout
+        self.set_remote(**relation_info)
+                
